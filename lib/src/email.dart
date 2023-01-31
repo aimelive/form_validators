af368db29c61c6c7271804642244ffe43af41323
aimelive <aimendayambaje24@gmail.com>
2023-01-31 14:07:51 +0200

    Initial commit

---
+++ lib/src/email.dart
@@ -0,0 +1,34 @@
+import 'package:form_validators/form_validators.dart';
+
+enum EmailValidatorError { empty, invalid }
+
+const String _kEmailPattern =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+class Email extends FormzInput<String, EmailValidatorError> {
+  const Email.pure() : super.pure('');
+  const Email.dirty([String value = '']) : super.dirty(value);
+
+  static final _regex = RegExp(_kEmailPattern);
+
+  @override
+  EmailValidatorError? validator(String value) {
+    if (_regex.hasMatch(value)) {
+      return null;
+    } else if (value.isEmpty) {
+      return EmailValidatorError.empty;
+    } else {
+      return EmailValidatorError.invalid;
+    }
+  }
+
+  static String? showEmailErrorMessage(EmailValidatorError? error) {
+    if (error == EmailValidatorError.empty) {
+      return "Empty email";
+    } else if (error == EmailValidatorError.invalid) {
+      return "Invalid email";
+    } else {
+      return null;
+    }
+  }
+}
---
+++ lib/src/password.dart
@@ -0,0 +1,29 @@
+import 'package:formz/formz.dart';
+
+enum PasswordValidatorError { empty, invalid }
+
+class Password extends FormzInput<String, PasswordValidatorError> {
+  const Password.pure() : super.pure('');
+  const Password.dirty([String value = '']) : super.dirty(value);
+
+  @override
+  PasswordValidatorError? validator(String value) {
+    if (value.isEmpty) {
+      return PasswordValidatorError.empty;
+    } else if (value.length < 6) {
+      return PasswordValidatorError.invalid;
+    } else {
+      return null;
+    }
+  }
+
+  static String? showPasswordErrorMessage(PasswordValidatorError? error) {
+    if (error == PasswordValidatorError.empty) {
+      return "Empty password";
+    } else if (error == PasswordValidatorError.invalid) {
+      return "Invalid password";
+    } else {
+      return null;
+    }
+  }
+}
